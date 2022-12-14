399bb0fbbbffaaba08588626210f531f7cbe0bc2
jorno sittrop <35764708+jornoS@users.noreply.github.com>
2022-12-14 04:59:56 +0100

    comments added

--- web.php
+++ web.php
@@ -14,8 +14,11 @@ use App\Http\Controllers\TaakLijstController;
 |
 */
 
+//main pagina door sturen
 Route::get('/', [TaakLijstController::class, 'index'] );
 
+//route voor de taak opslaan in database
 Route::post('/saveTaakRoute', [TaakLijstController::class, 'saveTaak'])->name('saveTaak');
 
+//route voor om een taak compleet te maken in database
 Route::post('/CompleetRoute/{id}', [TaakLijstController::class, 'Compleet'])->name('Compleet');
