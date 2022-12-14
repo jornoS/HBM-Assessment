0d44883f8fab6f6c57c6888cd07930ad7f295e73
jorno sittrop <35764708+jornoS@users.noreply.github.com>
2022-12-14 05:03:49 +0100

    comments added

--- TaakLijstController.php
+++ TaakLijstController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+//om dingen uit andere files te halen
 use Illuminate\Http\Request;
 use App\Models\TaakLijst;
 
 class TaakLijstController extends Controller
 {
+    // funtie om infromatie uit de databse naar de welcome pagina te sturen
     public function index(){
         return view('welcome', ['TakenLijst' => TaakLijst::where('is_compleet', 0)->get()]);
     }
+    
+    // functie om een taak compleet te zetten in de database
     public function Compleet($id){
 
         $TaakLijst = TaakLijst::find($id);
@@ -19,6 +23,7 @@ class TaakLijstController extends Controller
         return redirect('/');
     }
 
+    // functie om de informatie van de taak opteslaan
     public function saveTaak(Request $request){
 
         $nieuweTaak = new TaakLijst;
